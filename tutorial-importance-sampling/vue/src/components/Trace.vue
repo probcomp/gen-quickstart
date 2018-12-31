93a1c44f455feb904cf04c1b90decd280db76e20
Marco Cusumano-Towner <imarcoam@gmail.com>
2018-12-30 23:40:14 -0500

    checkpoint

---
+++ tutorial-importance-sampling/vue/src/components/Trace.vue
@@ -0,0 +1,57 @@
+<template>
+  <svg :height="size.w" :width="size.w">
+
+	<!-- Draw obstacles  -->
+	<polygon v-for="(obs, i) in trace.scene.obstacles" :key="i"
+			 :points="svgPoints(obs)" style="fill:rgba(100,100,100,1)" />
+
+	<!-- If there is a path, draw the gray line segments that make it up -->
+	<g v-if="trace.path.length > 1">
+			<line v-for="n in trace.path.length-1" :key="n"
+				  :x1="xLogicalToPixel(trace.path[n-1].x)" 
+				  :y1="yLogicalToPixel(trace.path[n-1].y)"
+				  :x2="xLogicalToPixel(trace.path[n].x)"   
+				  :y2="yLogicalToPixel(trace.path[n].y)" 
+				  style="stroke-width:2;stroke:rgba(100,100,100,0.4)"/>
+		</g>
+
+		<!-- Goal (end) point -->
+		<!-- <circle :cx="xLogicalToPixel(t.stop.x)" 
+				:cy="yLogicalToPixel(t.stop.y)" 
+				r="3" fill="rgba(255,0,0,0.5)" /> -->
+	
+	<!-- Start point (same for all traces) -->
+	<!-- <circle :cx="xLogicalToPixel(info.start.x)" 
+			:cy="yLogicalToPixel(info.start.y)" 
+			r="3" fill="blue" /> -->
+	
+	<!-- Observations (same for all traces) -->
+	<!-- <circle v-for="(m, i) in info.measurements" 
+			:key="i" 
+			:cx="xLogicalToPixel(m.x)" 
+			:cy="yLogicalToPixel(m.y)" 
+			r="2" fill="black" /> -->
+</svg>
+</template>
+
+<script>
+export default {
+  name: 'Trace',
+  props: ['info', 'trace', 'size'],
+  methods: {
+    xLogicalToPixel(x) {
+      return (x - this.trace.scene.xmin) / (this.trace.scene.xmax-this.trace.scene.xmin) * this.size.w
+    },
+    yLogicalToPixel(y) {
+      return this.size.w - ((y - this.trace.scene.ymin) / (this.trace.scene.ymax-this.trace.scene.ymin)) * this.size.w
+    },
+    xPixelToLogical(x) {
+      return x/(this.size.w) * (this.trace.scene.xmax - this.trace.scene.xmin) + this.trace.scene.xmin
+    },
+    svgPoints(obs) {
+      var self = this;
+      return obs.vertices.map(function (pt) { return self.xLogicalToPixel(pt.x) + "," + self.yLogicalToPixel(pt.y); }).join(' ')
+    }
+  }
+}
+</script>
