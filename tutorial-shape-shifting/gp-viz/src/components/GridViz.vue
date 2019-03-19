41308f0a9fadf663e3db9f4f0afd8a0d14ec1c87
alex-lew <alexl.mail@gmail.com>
2019-03-19 14:17:10 -0400

    Add shape-shifting inference notebook

---
+++ tutorial-shape-shifting/gp-viz/src/components/GridViz.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+    <h1>Traces</h1>
+    <div id="traces">
+        <div v-for="(t, tId) in traces" :key="tId">
+            <Trace :trace="t" :info="info" :size="traceSize" :tId="tId">
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import Trace from './Trace.vue'
+
+export default {
+  name: 'GridViz',
+  components: {Trace},
+  data() {
+    return {
+      windowSize: {height: window.innerHeight, width: window.innerWidth},
+    }
+  },
+  props: ['traces', 'info'],
+  computed: {
+      traceSize() {
+        let numTraces = Object.keys(this.traces).length
+        let perRow = numTraces < 5 ? numTraces : 5
+        let numRows = Math.ceil(numTraces / perRow)
+        return {h: this.windowSize.height/numRows, w: Math.min(500, (this.windowSize.width-10)/perRow)}
+      }
+  },
+  mounted () {
+    this.$nextTick(() => {
+        window.addEventListener('resize', () => {
+            this.windowSize = {height: window.innerHeight, width: window.innerWidth};
+        })
+    })
+  }
+}
+</script>
+
+<style>
+html, body {
+    margin-left: 0;
+    margin-right: 0;
+}
+</style>
+
+<style scoped>
+h1 {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    text-align: center;
+}
+#traces {
+    display: flex;
+    flex-flow: row wrap;
+}
+
+</style>
